2ae3186c53a94a76d8da0133f345152ecdc8a52c
youmix <hatena66@gmail.com>
2022-09-06 18:29:20 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const line = require('@line/bot-sdk');
+
+const config = {
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+};
+
+const app = express();
+app.post('/webhooks/line', line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
+  );
+});
+
+const client = new line.Client(config);
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text,
+  });
+}
+
+app.listen(3000);
